994f245b9c0db8fef378cb9bd829f6bf84cb4bd3
Nitsan-BYS <nitsan.b5@gmail.com>
2023-03-04 16:07:40 +0200

    fixed imaging on Login page & responsiveness added

--- final-project-ui/src/Components/Additionals/Header/Header.js
+++ final-project-ui/src/Components/Additionals/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 const Header = (props) => {
    return (
       <>
-         <header>
+         <header className={styles.title}>
             <h1>{props.title}</h1>
             <h2>{props.secondaryTitle}</h2>
          </header>
--- final-project-ui/src/Components/LoginPage/Login.js
+++ final-project-ui/src/Components/LoginPage/Login.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { TextField, Button, FormControl, Typography, Link } from '@mui/material';
+import {
+   TextField,
+   Button,
+   FormControl,
+   Typography,
+   Link,
+} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import Logo from '../Additionals/Logo/Logo';
 import Header from '../Additionals/Header/Header';
 import styles from './Login.module.css';
@@ -7,67 +15,91 @@ import PageContainer from '../Additionals/Container/PageContainer';
 import Divider from '../Additionals/Divider/Divider';
 
 const Login = (props) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
-  }
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(`Email: ${email}, Password: ${password}`);
+   };
 
-  return (
-    <>
-    <PageContainer> 
-    <Logo/>
-    <Header title="Welcome Back!"/>
-    <form onSubmit={handleSubmit}>
-      <FormControl sx={{ width: '40%'}}>
-      <TextField className={styles.input}
-        label="Email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        margin="normal"
-        variant="outlined"
-        required
-      />
-      <TextField className={styles.input}
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        margin="normal"
-        variant="outlined"
-        required
-      />
-      <Button
-        className={styles.button}
-        type="submit"
-        variant="contained"
-        color="primary"
-        sx={{mt:3, ml:14, width: '50%'}}
-        style={{
-          borderRadius: 20,
-          backgroundColor: '#2471A3'
-        }}
-      >
-        Login
-      </Button>
-      </FormControl>
-    </form>
-    <Typography style={{fontSize: "small"}} sx={{mt:2, mb:4}}><b>Don't Have An Account?</b> <Link>Sign Up</Link></Typography>
-    <Divider title="Sign In With"/>
-    <div className={styles.flexbox}>
-      <a href='#'>
-        <div className={styles.facebook_icon}></div>
-      </a>
-      <a href='#'>
-        <div className={styles.google_icon}></div>
-      </a>
-    </div>
-    </PageContainer>
-    </>
-  );
+   const theme = useTheme();
+   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+   return (
+      <>
+         <PageContainer>
+            <Logo />
+            <Header title='Welcome Back!' />
+            <form onSubmit={handleSubmit}>
+               <FormControl
+                  sx={isSmallScreen ? { width: '50%' } : { width: '40%' }}
+               >
+                  <TextField
+                     className={styles.input}
+                     label='Email'
+                     type='email'
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                     margin='normal'
+                     variant='outlined'
+                     required
+                  />
+                  <TextField
+                     className={styles.input}
+                     label='Password'
+                     type='password'
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                     margin='normal'
+                     variant='outlined'
+                     required
+                  />
+                  <Button
+                     className={styles.button}
+                     type='submit'
+                     variant='contained'
+                     color='primary'
+                     sx={
+                        isSmallScreen
+                           ? { mt: 4, ml: 2, width: '80%' }
+                           : { mt: 3, ml: 14, width: '50%' }
+                     }
+                     style={{
+                        borderRadius: 20,
+                        backgroundColor: '#2471A3',
+                     }}
+                  >
+                     Login
+                  </Button>
+               </FormControl>
+            </form>
+            <Typography style={{ fontSize: 'small' }} sx={{ mt: 2, mb: 4 }}>
+               <b>Don't Have An Account?</b>{' '}
+               <Link
+                  href='/'
+                  sx={{
+                     textDecoration: 'none',
+                     '&:hover': {
+                        textDecoration: 'underline',
+                     },
+                  }}
+               >
+                  Sign Up
+               </Link>
+            </Typography>
+            <Divider title='Sign In With' />
+            <div className={styles.flexbox}>
+               <a href='#'>
+                  <div className={styles.facebook_icon}></div>
+               </a>
+               <a href='#'>
+                  <div className={styles.google_icon}></div>
+               </a>
+            </div>
+         </PageContainer>
+      </>
+   );
 };
 
 export default Login;
